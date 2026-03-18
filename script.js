3ae263cbd231c68c4a8cc54b05b5de1281e14585
syed kareem <kareemsyed.official@gmail.com>
2026-03-18 23:11:27 +0530

    v-6

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function addStudent() {
     let name = document.getElementById("name").value;
-    let studentClass = document.getElementById("class").value;
+    let studentBranch = document.getElementById("Branch").value;
     let marks = document.getElementById("marks").value;
 
-    if (name === "" || studentClass === "" || marks === "") {
+    if (name === "" || studentBranch === "" || marks === "") {
         alert("Please fill all fields");
         return;
     }
@@ -17,11 +17,11 @@ function addStudent() {
     let cell3 = row.insertCell(2);
 
     cell1.innerHTML = name;
-    cell2.innerHTML = studentClass;
+    cell2.innerHTML = studentBranch;
     cell3.innerHTML = marks;
 
     // Clear input fields
     document.getElementById("name").value = "";
-    document.getElementById("class").value = "";
+    document.getElementById("Branch").value = "";
     document.getElementById("marks").value = "";
 }
